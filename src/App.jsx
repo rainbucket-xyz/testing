461b2a6782989637bcf53bde76f5038f4f3c349d
ramlang <rachelelang7672@gmail.com>
2024-02-19 14:31:39 -0800

    installed/livecycle-sdk

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import LivecycleSdk from '@livecycle/sdk'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  
+  useEffect(() => {
+    LivecycleSdk.init()
+  });
 
   return (
     <>
